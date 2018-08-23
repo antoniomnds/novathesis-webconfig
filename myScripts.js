dcc315ce7591773b608c878200a21c4e9b903dab
Antonio Santos <amn.santos@campus.fct.unl.pt>
2018-08-23 20:04:30 +0100

    Update navigation bar.
    * When the screen is less than 850 pixels wide, hide all links, except for the first one ("Home") and the link that expands and closes the topnav (hamburguer icon).

--- myScripts.js
+++ myScripts.js
@@ -1,11 +1,20 @@
-function showFormData(element_id){
+function showFormData(element_id) {
     var x = document.forms[element_id]; // document.forms returns all <form> elements 
     var text = "";
-    for(var i = 0; i < x.length; i++) {
-        text += x.elements[i].value + "<br/>";    // append to text
+    for (var i = 0; i < x.length; i++) {
+        text += x.elements[i].value + "<br/>"; // append to text
     }
     document.getElementById("form_content").innerHTML = text;
 }
 
 // var x = document.getElementsByTagName("form");  // object collection with all document forms
 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myNav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
