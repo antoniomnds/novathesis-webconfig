49c53173e882cdd1d144a5c8b565b19a4ef45b6a
Antonio Santos <amn.santos@campus.fct.unl.pt>
2018-08-28 23:06:25 +0100

    Add slideshow with document covers

--- myScripts.js
+++ myScripts.js
@@ -1,7 +1,8 @@
 function showFormData(element_id) {
     var x = document.forms[element_id]; // document.forms returns all <form> elements 
     var text = "";
-    for (var i = 0; i < x.length; i++) {
+    var i;
+    for (i = 0; i < x.length; i++) {
         text += x.elements[i].value + "<br/>"; // append to text
     }
     document.getElementById("form_content").innerHTML = text;
@@ -18,3 +19,42 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+
+
+var slideIndex = 1;
+/* To avoid that document.getElementsByClassName(...) returns an empty array due to the DOM not being loaded yet */
+document.addEventListener("DOMContentLoaded", function(event) { 
+    showSlides(slideIndex);
+});
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
